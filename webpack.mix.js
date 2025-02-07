e400d4186b6e55bd1e24e2d0c074abc3a3c2df0d
Asta Rem <asta.rem@hotmail.com>
2025-02-07 19:51:09 +0200

    initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,7 @@
+let mix = require('laravel-mix');
+
+mix
+.js('src/index.js', 'public').setPublicPath('public')
+.sass('src/styles.scss', 'public')
+//paims app.js is src, transpiliuos ir perkels i public
+.copy('src/index.html', 'public');
